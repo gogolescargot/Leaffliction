6a2db5e3f10998bcb8fd8a4b390fb685bd707f34
bac0nb0yy <kyuma@pm.me>
2025-05-27 16:25:35 +0200

    Added images and better parsing/handling errors with argparse

--- src/Distribution.py
+++ src/Distribution.py
@@ -1,46 +1,59 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Distribution.py                                    :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: ggalon <ggalon@student.42lyon.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/05/27 13:59:07 by ggalon            #+#    #+#              #
-#    Updated: 2025/05/27 15:42:14 by ggalon           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import os
-import sys
+import signal
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
-def distribution():
-	if len(sys.argv) != 2:
-		print(f"Usage: python {sys.argv[0]} <path>", file=sys.stderr)
-		sys.exit(1)
 
-	path = sys.argv[1]
+DEFAULT_LOCATION_DATASET = "images/"
 
-	categories = {}
 
-	for dirpath, _, filenames in os.walk(path):
-		if os.path.abspath(dirpath) == os.path.abspath(path):
-			continue
-		categorie = os.path.basename(dirpath)
-		categories[categorie] = len(filenames)
+def parse_args():
+    parser = ArgumentParser(
+        prog="Distribution",
+        description="Plot distribution for each plant.",
+    )
 
-	plt.figure(figsize=(6, 3))
+    parser.add_argument(
+        "--path",
+        type=str,
+        default=DEFAULT_LOCATION_DATASET,
+        help=f"Path to the input dataset. Defaults to '{DEFAULT_LOCATION_DATASET}' if not specified.",
+    )
 
-	plt.subplot(131)
-	plt.pie(categories.values(), labels=categories.keys())
+    return parser.parse_args()
 
-	plt.subplot(132)
-	plt.bar(categories.keys(), categories.values())
 
-	plt.suptitle("Class Distribution")
-	plt.show()
+def main():
+    args = parse_args()
 
-	return
+    try:
+        signal.signal(
+            signal.SIGINT,
+            lambda *_: (print("\033[2DLeaffliction: CTRL+C sent by user."), exit(1)),
+        )
+
+        categories = {}
+
+        for dirpath, _, filenames in os.walk(args.path):
+            if os.path.abspath(dirpath) == os.path.abspath(args.path):
+                continue
+            categorie = os.path.basename(dirpath)
+            categories[categorie] = len(filenames)
+
+        plt.figure(figsize=(6, 3))
+
+        plt.subplot(131)
+        plt.pie(categories.values(), labels=categories.keys())
+
+        plt.subplot(132)
+        plt.bar(categories.keys(), categories.values())
+
+        plt.suptitle("Class Distribution")
+        plt.show()
+
+    except Exception as ex:
+        print(f"Unexpected error occured : {ex}")
+
 
 if __name__ == "__main__":
-	distribution()
+    main()
